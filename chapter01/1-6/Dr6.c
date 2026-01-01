09c9dbf8830827d5c025b746fdcec9fa9571cb94
Juna1013 <jmeron.530@gmail.com>
2026-01-01 20:04:46 +0900

    1-6, 1-7の実装

---
+++ chapter01/1-6/Dr6.c
@@ -0,0 +1,19 @@
+// ユークリッドの互除法（改良版）
+#include <stdio.h>
+
+int main(void) {
+    int a, b, m, n, k;
+
+    printf("2つの整数を入力してください");
+    scanf("%d %d", &a, &b);
+
+    m = a; n = b;
+    do {
+        k = m % n;
+        m = n; n = k;
+    } while (k != 0);
+
+    printf("最大公約数 = %d\n", m);
+
+    return 0;
+}
