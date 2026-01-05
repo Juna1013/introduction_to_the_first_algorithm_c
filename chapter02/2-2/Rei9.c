908fd5a44b8ec5e22aea2aba1badd91030bacfe3
Juna1013 <jmeron.530@gmail.com>
2026-01-05 15:13:03 +0900

    2-2 数値積分の実装

---
+++ chapter02/2-2/Rei9.c
@@ -0,0 +1,27 @@
+// 台形則による定積分
+#include <stdio.h>
+#include <math.h>
+
+#define f(x) (sqrt(4 - (x) * (x))) // 被積分関数
+
+int main(void) {
+    int k;
+    double a, b, n, h, x, s, sum;
+
+    printf("積分区間 A, B ? ");
+    scanf("%lf %lf", &a, &b);
+
+    n = 50;
+    h = (b - a) / n;
+    x = a; s = 0;
+    for (k = 1; k <= n - 1; k++) {
+        x = x + h;
+        s = s + f(x);
+    }
+    sum = h * ((f(a) + f(b)) / 2+s);
+    printf("　　/%f\n", b);
+    printf("　　｜sqrt(4 - x*x) = %f\n", sum);
+    printf("　　/%f\n", a);
+
+    return 0;
+}
