3615fc9167912aebd29f888f1c5702f2e7a4e7c7
Juna1013 <jmeron.530@gmail.com>
2026-02-25 13:51:33 +0900

    3-3 線形探索と番兵

---
+++ chapter03/3-3/Dr20_1.c
@@ -0,0 +1,26 @@
+// 線形探索法（番兵を立てる）
+#include <stdio.h>
+#include <string.h>
+
+#define N 10 // データ数
+
+int main(void) {
+    struct girl {
+        char name[20];
+        int age;
+    } a[N+1] = {"Ann", 18, "Rolla", 19, "Nancy", 16, "Eluza", 17,
+                "Juliet", 18, "Machilda", 20, "Emy", 15, "Candy", 16,
+                "Ema", 17, "Mari", 18};
+    char key[20];
+    int i;
+
+    printf("検索する data ? "); scanf("%s", key);
+
+    strcpy(a[N].name, key);
+    i = 0;
+    while (strcmp(key, a[i].name) != 0) i++;
+    if (i < N) printf("%s %d\n", a[i].name, a[i].age);
+    else printf("見つかりませんでした\n");
+
+    return 0;
+}
