4bfd8112c8e14249cf5e661a1c18bfa245607767
Juna1013 <jmeron.530@gmail.com>
2026-02-27 13:18:52 +0900

    4-4 ハノイの塔

---
+++ chapter04/4-4/Rei29.c
@@ -0,0 +1,22 @@
+// ハノイの塔の最適解
+#include <stdio.h>
+
+void hanoi(int, char, char, char);
+
+int main(void) {
+    int n;
+    printf("円盤の枚数 ? ");
+    scanf("%d", &n);
+
+    hanoi(n, 'a', 'b', 'c');
+
+    return 0;
+}
+
+void hanoi(int n, char a, char b, char c) {
+    if (n > 0) {
+        hanoi(n-1, a, c, b);
+        printf("%d 番の円盤を %c から %c に移動\n", n, a, b);
+        hanoi(n-1, c, b, a);
+    }
+}
